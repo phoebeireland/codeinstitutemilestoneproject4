b6f26827c41e2d04e333381807dc83dba637bf56
Phoebe Ireland <pireland2016@gmail.com>
2021-11-12 22:13:59 +0000

    update context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,12 +1,25 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
     product_count = 0
-    
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
     grand_total = total * Decimal(settings.STANDARD_TAX / 100)
     
     context = {
@@ -15,5 +28,4 @@ def bag_contents(request):
         'product_count': product_count,
         'grand_total': grand_total,
     }
-
     return context
