568aec46ba37bc4aba7ef94983d78b89911ec1ae
Phoebe Ireland <pireland2016@gmail.com>
2021-12-03 14:39:03 -0500

    add basic filling to the blog page

--- condolences/urls.py
+++ condolences/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
 from . import views
+from django.urls import path
 
 urlpatterns = [
-    path('', views.view_condolences, name='condolences'),
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
--- condolences/views.py
+++ condolences/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
-from django.contrib import messages
+from django.views import generic
+from .models import Post
 
-def view_condolences(request):
-    """ A view that renders the bag contents page """
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
 
-    return render(request, 'condolences/condolences.html')
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
