20bbccbc502de7dfb4b4a3a75ba9509063f0dfb2
Phoebe Ireland <pireland2016@gmail.com>
2021-12-03 14:59:30 -0500

    not really sure. hopefully this doesn't break everything even more

--- condolences/urls.py
+++ condolences/urls.py
@@ -1,7 +1,6 @@
-from . import views
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('', views.view_condolences, name='condolences'),
 ]
--- condolences/views.py
+++ condolences/views.py
@@ -1,16 +1,5 @@
-from django.views import generic
-from .models import Post
 from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
-
-class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
-
-class PostDetail(generic.DetailView):
-    model = Post
-    template_name = 'post_detail.html'
-
 
 def view_condolences(request):
     """ A view that renders the bag contents page """
