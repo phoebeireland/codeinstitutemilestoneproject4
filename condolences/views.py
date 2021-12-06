a884e59fa3c96669a8e44c186e4f2cd8f33e0f9c
Phoebe Ireland <pireland2016@gmail.com>
2021-12-06 13:36:44 -0500

    delete the condolences.html page as it wasn't being used at all and update in views.py

--- condolences/views.py
+++ condolences/views.py
@@ -3,7 +3,7 @@ from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'condolences.html'
+    template_name = 'post_list.html'
 
 
 class PostDetail(generic.DetailView):
