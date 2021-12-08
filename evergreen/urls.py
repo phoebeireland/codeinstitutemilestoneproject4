68053af0cd3661f44c7506e99b83f4c2ee523a26
Phoebe Ireland <pireland2016@gmail.com>
2021-12-08 14:16:23 -0500

    add about app for the about page and link the urls

--- evergreen/urls.py
+++ evergreen/urls.py
@@ -31,4 +31,5 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     path('condolences/', include('condolences.urls')),
+    path('about/', include('about.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
