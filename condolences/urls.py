87b8d3736060a2190901ecbb242290f4604b3ef0
Phoebe Ireland <pireland2016@gmail.com>
2021-12-13 12:13:05 -0500

    replace slugs with post ids

--- condolences/urls.py
+++ condolences/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='condolences'),
-    # path('<slug:slug>/', views.PostDetail.as_view(), name='post_details'),
-    path('<slug:slug>/', views.post_details, name='post_details'),
+    path('<int:id>/', views.post_details, name='post_details'),
+    path('add/', views.add_post, name='add_post'),
+    path('edit/<int:id>/', views.edit_post, name='edit_post'),
+    path('delete/<int:id>/', views.delete_post, name='delete_post'),
 ]
